200a2cf09598c01cc0888b98c972448309f08994
Zach Smelcer <zacaronithesmelcer@icloud.com>
2020-11-11 15:30:48 -0600

    Installed mongojs through npm in the terminal

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 3060
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -10,7 +10,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localinstance/fitness", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
